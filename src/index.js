6c629ab02ad33cd70fc13220444dd7d1c1537599
SobolevYuri <yura.sobolev.91@gmail.com>
2018-02-22 16:38:32 +0300

    My 4th task (quad_eq)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+  var eq=equation.split(' ');
+var a=+eq[0], b=+(eq[3]+eq[4]),c=+(eq[7]+eq[8]);
+var D=Math.pow(b,2) - 4*a*c;
+var result=[Math.round(((-1)*b+Math.sqrt(D))/(2*a)),Math.round(((-1)*b-Math.sqrt(D))/(2*a))];
+
+return result.sort(function(l,r){return l-r;});
 }
